55f271b3776684c84ffc64e2dfc8bc4752666f10
Roqia Hany <roqiahani5@gmail.com>
2024-01-27 05:30:25 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+// window.alert('hello external');
+// document.getElementById('demo').innerHTML = 'HELLO ROQA ';
+// console.log('hello DEV');
+
+// var userName = 'roqia ';
+// userName = 'roqa';
+// userName += 'hany';
+// console.log(userName);
+
+// var user_name = window.prompt('enter your name');
+// console.log(user_name);
+
+// document.getElementById('demo').innerHTML = user_name;
+// var userAge;
+// console.log(typeof userAge);
+
+// console.log(typeof undefined);
+
+var num1 = window.prompt('enter num1');
+var num2 = window.prompt('enter num2');
+
+var result = Number(num1) + Number(num2);
+
+console.log(result);
